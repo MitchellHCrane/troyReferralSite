fbcb41c0591f95ec9ca4f988c25e783eb57c8fe9
Mitchell Crane <mitchellhcrane@gmail.com>
2023-08-15 16:17:03 -0600

    icon updates

--- src/Components/GetStarted.js
+++ src/Components/GetStarted.js
@@ -1,5 +1,9 @@
 import "../css/getStarted.css";
-import { CurrencyDollarIcon } from "@heroicons/react/24/outline";
+import {
+  CheckBadgeIcon,
+  ClipboardDocumentListIcon,
+  CurrencyDollarIcon,
+} from "@heroicons/react/24/outline";
 
 function GetStarted() {
   return (
@@ -38,7 +42,7 @@ function GetStarted() {
         <div className="programDetails topSpacing">
           <div className="programDetailsDiv">
             <span className="heroIconSpan">
-              <CurrencyDollarIcon className="heroIcon" />
+              <CheckBadgeIcon className="heroIcon" />
             </span>
             <h2 className="programDetailsH2">Eligibility</h2>
           </div>
@@ -50,7 +54,7 @@ function GetStarted() {
         <div className="programDetails topSpacing">
           <div className="programDetailsDiv">
             <span className="heroIconSpan">
-              <CurrencyDollarIcon className="heroIcon" />
+              <ClipboardDocumentListIcon className="heroIcon" />
             </span>
             <h2 className="programDetailsH2">Rules</h2>
           </div>
