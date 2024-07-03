eb27200adbd0cb3707a1ad3134a232318620f25c
MitchellHCrane <mitchellhcrane@gmail.com>
2024-07-03 16:15:48 -0600

    add video

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -15,19 +15,24 @@ function Profile() {
         />
       </div>
       <div className="profile-grid">
-        <div className="columnPic">
-          {/* <img
-            className="troyProfilePic"
-            src="../images/steveJones.jpg"
-            alt="First Class"
-          /> */}
-        </div>
+        <div className="columnPic"></div>
         <div className="center-div">
           <h1 className="profileName">We Are Hiring Originators</h1>
           <p className="profileP">
             The Partner/Recruiting Referral Program is a great way you can make
             extra cash!
           </p>
+          <div className="videoContainer">
+            <iframe
+              title="video"
+              width="560"
+              height="315"
+              src="https://www.youtube.com/embed/dmEV-6MeL7Y"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
           <div className="buttonBlueDiv">
             <a href="#getStarted" className="buttonBlue">
               Learn More
